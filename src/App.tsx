17fcc2a55c57f818bfe98a072ae3ca384dfba77b
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-09-13 15:34:19 +0300

    continents listed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+  const [continents, setContinents] = useState<any[]>([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const getCountries = async () => {
+    const options = {
+      method: 'POST',
+      url: 'https://countries.trevorblades.com/',
+      headers: {
+        'content-type': 'application/json',
+      },
+      data: {
+        query: `{
+          continents {
+            name
+            code
+          }
+        }`
+      }
+    };
+    
+    try {
+      const response = await axios.request(options);
+      const res = response.data;
+      setContinents(res.data.continents);
+      console.log("res:", res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      Dataguess Assignment for Junior Frontend Developer 
+      {continents.map((item) => (
+        <div key={item.code}>{item.name}</div>
+      ))}
     </div>
   );
 }
